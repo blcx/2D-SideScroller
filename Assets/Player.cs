012e023e08c5e012731a45042220311e13010dbe
coderx <36203292+blcx@users.noreply.github.com>
2020-06-29 23:20:52 +0530

    Better jump 2D like Mario

---
+++ Assets/Player.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [Range(1, 10)]
+    public float jumpVelocity;    
+    Rigidbody2D rb;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+       rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Jump();
+        BetterJump();
+        
+    }
+
+
+
+
+
+
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = Vector2.up * jumpVelocity;
+        }
+    }
+
+
+
+
+    void BetterJump()
+    {
+        if (rb.velocity.y < 0) //if falling down
+        {
+            //faster fall
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump")) //if jumping up
+        {
+
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+
+
+        }
+    }
+
+
+
+
+
+
+
+}
